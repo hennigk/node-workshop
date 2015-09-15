58b8d86587a65d4e7e93cb6a088b88f06b70ddf3
hennigk <kayla.hennig@gmail.com>
2015-09-15 17:26:28 +0000

    location of ISS

---
+++ iss.js
@@ -0,0 +1,14 @@
+//a program that displays the location of the space station!
+
+var request = require('request');
+request('http://api.open-notify.org/iss-now.json', function (error, response, body) {
+  if (error) {
+      console.log("there was an error!! Error = " + error)
+  }
+  if (!error && response.statusCode == 200) {
+        var bodyObject = JSON.parse(body);
+        console.log("The ISS is in the following position:");
+        console.log("Latitude: " + bodyObject.iss_position.latitude)
+        console.log("Longitude: " + bodyObject.iss_position.longitude)
+  }
+})
